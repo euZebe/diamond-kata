a1814daa18e25329aeacd324a108aa05ad5c61ef
Jean MARTINEAU <jmartine@cnes-isis.toulouse.atos.net>
2018-03-09 14:14:21 +0000

    implement generic processing

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-function getParam() {
-  const param = process.argv[2];
-  if (!param || !param.match(/^[A-Za-z]$/)) {
-    throw 'One parameter (composed of one letter) is expected.';
-  }
-  return param;
-}
+// function getParam() {
+//   const param = process.argv[2];
+//   if (!param || !param.match(/^[A-Za-z]$/)) {
+//     throw 'One parameter (composed of one letter) is expected.';
+//   }
+//   return param;
+// }
 
 const PYRAMID_TIP = 'A';
 
+String.prototype.charCode = function() {
+  return this.charCodeAt(0);
+}
+
 export default (letter = PYRAMID_TIP) => {
-  return undefined;
+  const pyramidHeight = letter.charCode() - PYRAMID_TIP.charCode() + 1;
+  let result = '';
+  for(let level = 0 ; level < pyramidHeight ; level++) {
+    const nbSpacesBefore = pyramidHeight - level;
+    const currentLetter = String.fromCharCode(PYRAMID_TIP.charCode() + level);
+    result += '\n' + Array(nbSpacesBefore).join(' ') // spaces before
+      + currentLetter; // add letter
+    if (level != 0) {
+      const nbSpacesBetween = level * 2 - 1;
+      result += Array(nbSpacesBetween + 1).join(' ') // + 1 cause join feels only intervals
+        + currentLetter;
+    }
+  }
+
+  for (let level = pyramidHeight - 2 ; level >= 0 ; level--) {
+    const nbSpacesBefore = pyramidHeight - level;
+    const currentLetter = String.fromCharCode(PYRAMID_TIP.charCode() + level);
+    result += '\n' + Array(nbSpacesBefore).join(' ') // spaces before
+      + currentLetter; // add letter
+    if (level != 0) {
+      const nbSpacesBetween = level * 2 - 1;
+      result += Array(nbSpacesBetween + 1).join(' ') // + 1 cause join feels only intervals
+        + currentLetter;
+    }
+
+  }
+
+  return result;
 }
--- app.spec.js
+++ app.spec.js
@@ -2,6 +2,15 @@ import buildPyramid from './app';
 
 describe('buildPyramid', () => {
   test('should return a single A when no parameter given', () => {
-    expect(buildPyramid()).toBe('A');
+    expect(buildPyramid()).toBe('\nA');
   });
+
+  test('should return a A with two B when B is given', () => {
+    expect(buildPyramid('B')).toBe('\n A\nB B\n A');
+  });
+
+  test('should return a C diamond when C is given', () => {
+    expect(buildPyramid('C')).toBe('\n  A\n B B\nC   C\n B B\n  A');
+  });
+
 });
